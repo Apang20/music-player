67f8536a3febbfea8084191405d2b92a0346939b
github username <avelonpang@gmail.com>
2021-06-08 20:57:33 -0500

    final edits

--- script.js
+++ script.js
@@ -1,168 +1,40 @@
-// const audio = document.querySelector('audio');
-// const prevBtn = document.getElementById('btn-prev');
-// const playBtn = document.getElementById('btn-play');
-// const nextBtn = document.getElementById('btn-next');
-// const volumeBtn = document.getElementById('btn-volume');
-// const volumeControl = document.getElementById('volume-control');
-// const songImage = document.querySelector('.song-image');
-// const songTitle = document.querySelector('.song-title');
-// const songArtist = document.querySelector('.song-artist');
-// const durationTotal = document.querySelector('.duration_total');
-// const durationNow = document.querySelector('.duration_now');
-// const progressContainer = document.querySelector('.progress-container');
-// const progressBar = document.querySelector('.progress');
-
-// const playlist = [
-//     {name: 'strut', displayName: 'Strut', artist: 'Ocean Sunrise'},
-//     {name: 'dreams', displayName: 'Dreams', artist: 'Faded Rainbows'},
-//     {name: 'lemon', displayName: 'Lemon', artist: 'Citrus Drops'},
-//     {name: 'tenderness', displayName: 'Tenderness', artist: 'Marie Bolton'},
-//     {name: 'bloom', displayName: 'Bloom', artist: 'Pink Satin'},
-//     {name: 'phases', displayName: 'Phases', artist: 'Elizabeth Snow'},
-//     {name: 'arrival', displayName: 'Arrival', artist: 'Encrypted Cloud'},
-//     {name: 'journey', displayName: 'Journey', artist: 'The Shortcut'},
-//     {name: 'action', displayName: 'Action', artist: 'Wolf Pack 9'},
-//     {name: 'saturn', displayName: 'Saturn', artist: 'Solar Green'},
-//     {name: 'dawn', displayName: 'Dawn', artist: 'Pensive Silence'},
-//     {name: 'charming', displayName: 'Charming', artist: 'Lily Rose'},
-//     {name: 'moment', displayName: 'Moment', artist: 'Emily Glass'},
-//     {name: 'closer', displayName: 'Closer', artist: 'Smith Brown'},
-//     {name: 'avenue', displayName: 'Avenue', artist: 'Red Satin'},
-//     {name: 'galaxy', displayName: 'Galaxy', artist: 'OmniSounds'},
-//     {name: 'memories', displayName: 'Memories', artist: 'Bailey Rivers'},
-//     {name: 'rumble', displayName: 'Rumble', artist: 'The Plan'},
-//     {name: 'epic', displayName: 'Epic', artist: 'Metal Spear'}
-// ];
-
-
-// //Check if Playing
-// let isPlaying = false;
-// let musicIndex = 0;
-
-// // Play
-// function playMusic() {
-//   isPlaying = true;
-//   playBtn.classList.replace('fa-play', 'fa-pause');
-//   songImage.classList.add('image-rotate');
-//   audio.play();
-// }
-
-// //Pause
-// function pauseMusic() {
-//   isPlaying = false;
-//   playBtn.classList.replace('fa-pause', 'fa-play');
-//   songImage.classList.remove('image-rotate');
-//   audio.pause();
-// }
-
-// // Previous Music
-// function playPrevMusic() {
-//   musicIndex = musicIndex === 0 ? 18 : musicIndex - 1;
-//   loadMusic(playlist[musicIndex]);
-//   playMusic();
-// }
-
-// // Next Music
-// function playNextMusic() {
-//   musicIndex = musicIndex === 18 ? 0 : musicIndex + 1;
-//   loadMusic(playlist[musicIndex]);
-//   playMusic();
-// }
-
-
-// function setVolume(value) {
-//   audio.volume = value;
-// }
-
-// // //Update DOM
-// function loadMusic(music) {
-//   songImage.src = `img/${music.displayName}.jpg`;
-//   songTitle.textContent = music.displayName;
-//   songArtist.textContent = music.artist;
-//   audio.src = `music/${music.name}.mp3`;
-// }
-
-
-// function setDuration() {
-//   const musicDuration = audio.duration;
-//   const minute = Math.floor(musicDuration / 60);
-//   const second = ('0' + Math.floor(musicDuration % 60)).slice(-2);
-//   durationTotal.textContent = `${minute}:${second}`;
-// }
-// function updateProgress() {
-//   const musicCurrentTime = audio.currentTime;
-//   const minute = Math.floor(musicCurrentTime / 60);
-//   const second = ('0' + Math.floor(musicCurrentTime % 60)).slice(-2);
-//   durationNow.textContent = `${minute}:${second}`;
-//   progressBar.style.width = `${Math.floor((audio.currentTime * 100) / audio.duration)}%`;
-// }
-
-// function setProgressBar(event) {
-//   const width = this.clientWidth;
-//   const clickX = event.offsetX;
-//   const { duration } = audio;
-//   audio.currentTime = (clickX * duration) / width;
-// }
-
-// playBtn.addEventListener('click', () => {
-//   isPlaying ? pauseMusic() : playMusic();
-// });
-
-// prevBtn.addEventListener('click', () => {
-//   playPrevMusic();
-// });
-
-// nextBtn.addEventListener('click', () => {
-//   playNextMusic();
-// });
-
-// audio.onloadedmetadata = function () {
-//   setDuration();
-// };
-
-// audio.addEventListener('timeupdate', () => {
-//   updateProgress();
-// });
-
-// audio.addEventListener('ended', () => {
-//   playNextMusic();
-// });
-// progressContainer.addEventListener('click', setProgressBar);
-
-// loadMusic(playlist[musicIndex]);
-
 const audio = document.querySelector('audio');
-const prevBtn = document.getElementById('btn--prev');
-const playBtn = document.getElementById('btn--play');
-const nextBtn = document.getElementById('btn--next');
-const volumeBtn = document.getElementById('btn--volume');
+const prevBtn = document.getElementById('btn-prev');
+const playBtn = document.getElementById('btn-play');
+const nextBtn = document.getElementById('btn-next');
+const volumeBtn = document.getElementById('btn-volume');
 const volumeControl = document.getElementById('volume-control');
-const musicImage = document.querySelector('.music__image');
-const musicTitle = document.querySelector('.music__title');
-const musicArtist = document.querySelector('.music__artist');
-const durationTotal = document.querySelector('.duration__total');
-const durationNow = document.querySelector('.duration__now');
+const songImage = document.querySelector('.song-image');
+const songTitle = document.querySelector('.song-title');
+const songArtist = document.querySelector('.song-artist');
+const durationTotal = document.querySelector('.duration_total');
+const durationNow = document.querySelector('.duration_now');
 const progressContainer = document.querySelector('.progress-container');
 const progressBar = document.querySelector('.progress');
 
-//Playlist from youtube audio library
 const playlist = [
-  {
-      name: 'strut', 
-      displayName: 'Strut', 
-      artist: 'Ocean Sunrise'
-    },
+    
+    {name: 'strut', displayName: 'Strut', artist: 'Ocean Sunrise'},
     {name: 'dreams', displayName: 'Dreams', artist: 'Faded Rainbows'},
     {name: 'lemon', displayName: 'Lemon', artist: 'Citrus Drops'},
     {name: 'tenderness', displayName: 'Tenderness', artist: 'Marie Bolton'},
-
-  {
-    name: 'journey', 
-    displayName: 'Journey', 
-    artist: 'The Shortcut'
-}
-
+    {name: 'bloom', displayName: 'Bloom', artist: 'Pink Satin'},
+    {name: 'phases', displayName: 'Phases', artist: 'Elizabeth Snow'},
+    {name: 'arrival', displayName: 'Arrival', artist: 'Encrypted Cloud'},
+    {name: 'journey', displayName: 'Journey', artist: 'The Shortcut'},
+    {name: 'action', displayName: 'Action', artist: 'Wolf Pack 9'},
+    {name: 'saturn', displayName: 'Saturn', artist: 'Solar Green'},
+    {name: 'dawn', displayName: 'Dawn', artist: 'Pensive Silence'},
+    {name: 'charming', displayName: 'Charming', artist: 'Lily Rose'},
+    {name: 'moment', displayName: 'Moment', artist: 'Emily Glass'},
+    {name: 'closer', displayName: 'Closer', artist: 'Smith Brown'},
+    {name: 'avenue', displayName: 'Avenue', artist: 'Red Satin'},
+    {name: 'galaxy', displayName: 'Galaxy', artist: 'OmniSounds'},
+    {name: 'memories', displayName: 'Memories', artist: 'Bailey Rivers'},
+    {name: 'rumble', displayName: 'Rumble', artist: 'The Plan'},
+    {name: 'epic', displayName: 'Epic', artist: 'Metal Spear'}
 ];
+
 
 //Check if Playing
 let isPlaying = false;
@@ -172,7 +44,7 @@ let musicIndex = 0;
 function playMusic() {
   isPlaying = true;
   playBtn.classList.replace('fa-play', 'fa-pause');
-  musicImage.classList.add('music__image--rotate');
+  songImage.classList.add('image-rotate');
   audio.play();
 }
 
@@ -180,44 +52,44 @@ function playMusic() {
 function pauseMusic() {
   isPlaying = false;
   playBtn.classList.replace('fa-pause', 'fa-play');
-  musicImage.classList.remove('music__image--rotate');
+  songImage.classList.remove('image-rotate');
   audio.pause();
 }
 
 // Previous Music
 function playPrevMusic() {
-  musicIndex = musicIndex === 0 ? 4 : musicIndex - 1;
-  console.log(musicIndex);
+  musicIndex = musicIndex === 0 ? 18 : musicIndex - 1;
   loadMusic(playlist[musicIndex]);
   playMusic();
 }
 
 // Next Music
 function playNextMusic() {
-  musicIndex = musicIndex === 4 ? 0 : musicIndex + 1;
+  musicIndex = musicIndex === 18 ? 0 : musicIndex + 1;
   loadMusic(playlist[musicIndex]);
   playMusic();
 }
+
 
 function setVolume(value) {
   audio.volume = value;
 }
 
-//Update DOM
+// //Update DOM
 function loadMusic(music) {
-  musicImage.src = `img/${music.displayName}.jpg`;
-  musicTitle.textContent = music.displayName;
-  musicArtist.textContent = music.artist;
+  songImage.src = `img/${music.displayName}.jpg`;
+  songTitle.textContent = music.displayName;
+  songArtist.textContent = music.artist;
   audio.src = `music/${music.name}.mp3`;
 }
-// get duration
+
+
 function setDuration() {
   const musicDuration = audio.duration;
   const minute = Math.floor(musicDuration / 60);
   const second = ('0' + Math.floor(musicDuration % 60)).slice(-2);
   durationTotal.textContent = `${minute}:${second}`;
 }
-//update progress bar
 function updateProgress() {
   const musicCurrentTime = audio.currentTime;
   const minute = Math.floor(musicCurrentTime / 60);
@@ -259,3 +131,4 @@ audio.addEventListener('ended', () => {
 progressContainer.addEventListener('click', setProgressBar);
 
 loadMusic(playlist[musicIndex]);
+
